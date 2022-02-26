5d207e85a95978df40985e327fef9b561e7195b2
adelaadeoye <adeoye.adela@gmail.com>
2022-02-26 13:36:45 +0100

    signup signin page

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,41 +1,115 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import { Button,y } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { s3 } from "./AwsConfig";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import { Link } from "react-router-dom";
 
 function App() {
-  s3.listObjects(
-    {
-      Bucket: process.env.REACT_APP_BUCKET,
-    },
-    function (err, res) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(res);
+  const theme = createTheme();
+  const [imageUrl, setImageUrl] = useState([]);
+  useEffect(() => {
+    s3.listObjects(
+      {
+        Bucket: process.env.REACT_APP_BUCKET,
+      },
+      function (err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          setImageUrl(
+            res.Contents.splice(0, 500).map((item) => {
+              return { key: item.Key, status: "" };
+            })
+          );
+        }
       }
-    }
-  );
-  console.log(
-    "first",
-    `https://${process.env.REACT_APP_BUCKET}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/prod-exp13436-2020-01-08-at-04.24.38-9zijoye9dteugy6agooo506u3c6wrin920a99mavvv4z9mahkt7qbu6thl2l3v39.png`
-  );
+    );
+  }, []);
+  // console.log("imageUrl", imageUrl);
+  //     src={`https://${process.env.REACT_APP_BUCKET}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/${item.Key}?w=248&fit=crop&auto=format`}
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+    width: "250px",
+    fontSize:"1.8em"
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={`https://${process.env.REACT_APP_BUCKET}.s3.${process.env.REACT_APP_REGION}.amazonaws.com/prod-exp13436-2020-01-08-at-04.24.38-9zijoye9dteugy6agooo506u3c6wrin920a99mavvv4z9mahkt7qbu6thl2l3v39.png`} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          sx={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1644691568805-fd9a88e0f057?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY0NTg3NzAxNA&ixlib=rb-1.2.1&q=80&w=1080)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            display: "flex",
+            flexDirection: "column",
+            color: "white",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid sx={{alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            display: "flex",
+            flexDirection: "column",}}
+            container
+            maxWidth={700}
+           
+          >
+            <h1
+              style={{
+                textAlign: "center",
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",
+                alignContent:"center",
+                // backgroundColor: "white",
+                width: "100%"
+              }}
+            >
+              SWE Foam Take Home Challenge
+            </h1>
+            <p style={{ textAlign: "center" }}>
+              Here at Culture, we run all kinds of experiments for our clients.
+              These experiments run inside of reactors and can go for any amount
+              of time. This makes it tough to monitor them around the clock for
+              any problems. One of the common problems is that a reactor may
+              start “foaming”. This is where foam starts to build up towards the
+              top of the reactor and there is a risk of it spilling over. In
+              these cases we may need to add an anti-foaming agent to preserve
+              the run.
+            </p>
+            <h1 style={{ textAlign: "center" }}>
+              We want to create a classification system with you
+            </h1>
+            <Link to="/signin" style={{ textDecoration:"none"}}>
+            <ColorButton>Get Started</ColorButton>
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}/>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
