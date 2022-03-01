41fc760d7cfb38429b059483c654c11e55b54bc1
adelaadeoye <adeoye.adela@gmail.com>
2022-03-01 04:14:07 +0100

    made change to env in BE

--- firebase-backend/functions/APIs/images.js
+++ firebase-backend/functions/APIs/images.js
@@ -1,38 +1,38 @@
 const { db } = require("../util/admin");
-// const { s3 } = requir../../AwsConfigfig");
-// const axios = require("axios");
+const { s3 } = require("../util/AwsConfig");
+const axios = require("axios");
 
 //  here i create the image collection into the firebase database by getting the images url from the aws bucket
-// exports.createCollection = (req, res) => {
-//   db.collection("images")
-//     .doc("imageCollection")
-//     .get()
-//     .then((data) => {
-//       response.header("Access-Control-Allow-Origin", "*");
-//       if (response.json(data.data().data).length <= 0) {
-//         s3.listObjects(
-//           {
-//             Bucket: process.env.REACT_APP_BUCKET,
-//           },
-//           function (err, response) {
-//             console.log("i got here");
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               let data = res.Contents.map((item, index) => {
-//                 return { imageKey: item.Key, status: "", id: index, users: [] };
-//               });
-//               db.collection("images").doc("imageCollection").set({ data });
-//             }
-//           }
-//         );
-//       }
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       return res.status(500).json({ error: err.code });
-//     });
-// };
+exports.createCollection = (req, res) => {
+  db.collection("images")
+    .limit(1)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        console.log("collection is not created");
+        s3.listObjects(
+          {
+            Bucket: process.env.BUCKET,
+          },
+          function (err, response) {
+            if (err) {
+              console.log(err);
+            } else {
+              let data = response.Contents.map((item, index) => {
+                return { imageKey: item.Key, id: index, users: [] };
+              });
+              db.collection("images").doc("imageCollection").set({ data });
+            }
+          }
+        );
+      }
+      return res
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
 
 // get all images from the firebase db
 exports.getAllImages = (request, response) => {
@@ -68,43 +68,3 @@ exports.classifyImage = (request, response) => {
       });
     });
 };
-
-// exports.classifyImage = (request, response) => {
-//   db.collection("images")
-//     .doc("imageCollection")
-//     .get()
-//     .then((result) => {
-//       let data = result.data().data;
-//       result.data().data.some((item) => {
-//         if (item.id == 1) {
-//           data[1] = {
-//             id: 1,
-//             imageKey:
-//               "prod-exp13436-2020-01-08-at-15.01.15-sb6by9nohfpx416zeltkn4x3fi2h8mhzt5fzpuoxsdv23fx3o84bu7qjihakmwx5.png",
-//             status: "",
-//             users: [
-//               { email: "addd@a.com", status: "Foaming" },
-//               { email: "addsdddd@a.com", status: "Foaming" },
-//             ],
-//           };
-//           db.collection("images")
-//             .doc("imageCollection")
-//             .update({ data })
-//             .then((data) => {
-//               return response.json({ message: "Updated successfully", data });
-//             })
-//             .catch((err) => {
-//               console.error(err);
-//               return response.status(500).json({
-//                 error: err.code,
-//               });
-//             });
-//         }
-//       });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       return response.status(500).json({ error: err.code });
-//     });
-
-// };
--- firebase-backend/functions/index.js
+++ firebase-backend/functions/index.js
@@ -1,15 +1,17 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-const auth = require('./util/auth');
-
+require('dotenv').config()
 const cors = require('cors');
-
+const auth = require('./util/auth');
 app.use(cors());
-const { getAllImages, classifyImage } = require("./APIs/images");
+
+const { getAllImages, classifyImage,createCollection } = require("./APIs/images");
+
 
 const { loginUser,signUpUser } = require("./APIs/users");
 
 app.get("/images",auth, getAllImages);
+app.get("/create", createCollection);
 app.put('/images/:imageId',auth, classifyImage);
 app.post("/login", loginUser);
 app.post("/signup", signUpUser);
